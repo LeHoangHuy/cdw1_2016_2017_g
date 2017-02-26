c1a6ff3972142f50639e7ec468ac9debc18c7686
DinhCongThanh <congthanh1394@gmail.com>
2017-02-26 20:45:04 +0700

    Module của Thành
    Comit các module : 806, 807, 808, 809, 810

---
+++ blocks/808/808.php
@@ -0,0 +1,154 @@
+﻿<!DOCTYPE html>
+
+<html style="background-color: #f3f3f4;">
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opencart Super Deal Store</title>
+        <meta name="description" content="My Store" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="icon" type="image/x-icon" href="image/favicon.ico" />
+
+        <link href="framework/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen" />
+        <link href="framework/fontello/css/fontello.css" rel="stylesheet" type="text/css" />
+        <link href="framework/shortcodes/theme.shortcodes.css" rel="stylesheet" type="text/css" />
+
+
+
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/808.less', 'css/808.css');
+        ?>
+        <link href="css/808.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>
+        <div class="type-808">
+            <!-- MODULE 808-->	
+            <div class="module-808">
+                <div class="back">
+                    <div class="container">
+                        <div class="wrapper">
+                            <div class="row">
+                                <div class="vc_custom_7">
+                                    <div class="wpb_column vc_column_container vc_col-sm-12">
+                                        <div class="vc_column-inner vc_custom_1455028103515">
+                                            <div class="wpb_wrapper">
+                                                <div class="sc_section margin_top_null margin_bottom_large aligncenter" data-animation="animated fadeInUp normal">
+                                                    <div class="sc_section_inner">
+                                                        <h3 class="sc_title sc_title_underline sc_align_center margin_top_null">Our Club Coaches</h3>
+                                                        <div class="wpb_text_column wpb_content_element  vc_custom_7_1 text_uppercase">
+                                                            <div class="wpb_wrapper">
+                                                                <p class="ps"><span>Tennis Club is honored to work with the country’s league best tennis players that now can become your personal coach and help you win</span></p>
+                                                            </div>
+                                                        </div>
+                                                        <div id="sc_team_1274765886_wrap" class="sc_team_wrap">
+                                                            <div id="sc_team_1274765886" class="sc_team sc_team_style_team-3  margin_bottom_small" style="width:100%;">
+                                                                <div class="sc_columns columns_wrap">
+                                                                    <div class="col-md-6 column_padding_bottom">
+                                                                        <div id="sc_team_1274765886_1" class="sc_team_item sc_team_item_1 odd first columns_wrap">
+                                                                            <div class="sc_team_item_avatar column-2_5">
+                                                                                <img class="wp-post-image" width="370" height="370" alt="player-5.jpg" src="image/player-5-370x370.jpg">
+                                                                                <div class="sc_socials sc_socials_type_icons sc_socials_shape_square sc_socials_size_tiny">
+                                                                                    <div class="sc_socials_item"><a href="#" target="_blank" class="social_icons social_twitter"><span class="icon-twitter"></span></a></div>
+                                                                                    <div class="sc_socials_item"><a href="#" target="_blank" class="social_icons social_facebook"><span class="icon-facebook"></span></a></div>
+                                                                                    <div class="sc_socials_item"><a href="#" target="_blank" class="social_icons social_gplus"><span class="icon-gplus"></span></a></div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="sc_team_item_info column-3_5">
+                                                                                <h5 class="sc_team_item_title text_uppercase"><a href="#">Amy Scott</a></h5>
+                                                                                <div class="sc_team_item_position">Professional trainer</div>
+                                                                                <div class="sc_team_item_description acb">Ut enim ad minim veniam, quis nostrud exer tation ullamco laboris nisi ut aliquip ex ea commodo...</div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="col-md-6 column_padding_bottom">
+                                                                        <div id="sc_team_1274765886_2" class="sc_team_item sc_team_item_2 even columns_wrap">
+                                                                            <div class="sc_team_item_avatar column-2_5">
+                                                                                <img class="wp-post-image" width="370" height="370" alt="player-2.jpg" src="image/player-2-370x370.jpg">
+                                                                                <div class="sc_socials sc_socials_type_icons sc_socials_shape_square sc_socials_size_tiny">
+                                                                                    <div class="sc_socials_item"><a href="#" target="_blank" class="social_icons social_twitter"><span class="icon-twitter"></span></a></div>
+                                                                                    <div class="sc_socials_item"><a href="#" target="_blank" class="social_icons social_facebook"><span class="icon-facebook"></span></a></div>
+                                                                                    <div class="sc_socials_item"><a href="#" target="_blank" class="social_icons social_gplus"><span class="icon-gplus"></span></a></div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="sc_team_item_info column-3_5">
+                                                                                <h5 class="sc_team_item_title text_uppercase"><a href="#">Dale Adams</a></h5>
+                                                                                <div class="sc_team_item_position">Assistant</div>
+                                                                                <div class="sc_team_item_description acb">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim...</div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>    
+                                                                <div class="clearfix"></div>
+                                                                <div class="sc_columns columns_wrap">    
+                                                                    <div class="col-md-6 column_padding_bottom">
+                                                                        <div id="sc_team_1274765886_4" class="sc_team_item sc_team_item_4 even columns_wrap">
+                                                                            <div class="sc_team_item_avatar column-2_5">
+                                                                                <img class="wp-post-image" width="370" height="370" alt="image-21.jpg" src="image/image-21-370x370.jpg">
+                                                                                <div class="sc_socials sc_socials_type_icons sc_socials_shape_square sc_socials_size_tiny">
+                                                                                    <div class="sc_socials_item"><a href="#" target="_blank" class="social_icons social_twitter"><span class="icon-twitter"></span></a></div>
+                                                                                    <div class="sc_socials_item"><a href="#" target="_blank" class="social_icons social_facebook"><span class="icon-facebook"></span></a></div>
+                                                                                    <div class="sc_socials_item"><a href="#" target="_blank" class="social_icons social_gplus"><span class="icon-gplus"></span></a></div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="sc_team_item_info column-3_5">
+                                                                                <h5 class="sc_team_item_title text_uppercase"><a href="#">Lena James</a></h5>
+                                                                                <div class="sc_team_item_position">Club Manager</div>
+                                                                                <div class="sc_team_item_description acb">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim...</div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="col-md-6 column_padding_bottom">
+                                                                        <div id="sc_team_1274765886_3" class="sc_team_item sc_team_item_3 odd columns_wrap">
+                                                                            <div class="sc_team_item_avatar column-2_5">
+                                                                                <img class="wp-post-image" width="370" height="370" alt="player-3.jpg" src="image/player-3-370x370.jpg">
+                                                                                <div class="sc_socials sc_socials_type_icons sc_socials_shape_square sc_socials_size_tiny">
+                                                                                    <div class="sc_socials_item"><a href="#" target="_blank" class="social_icons social_twitter"><span class="icon-twitter"></span></a></div>
+                                                                                    <div class="sc_socials_item"><a href="#" target="_blank" class="social_icons social_facebook"><span class="icon-facebook"></span></a></div>
+                                                                                    <div class="sc_socials_item"><a href="#" target="_blank" class="social_icons social_gplus"><span class="icon-gplus"></span></a></div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="sc_team_item_info column-3_5">
+                                                                                <h5 class="sc_team_item_title text_uppercase"><a href="#">Eddie Grant</a></h5>
+                                                                                <div class="sc_team_item_position">Assistant</div>
+                                                                                <div class="sc_team_item_description acb">Ut enim ad minim veniam, quis nostrud exer tation ullamco laboris nisi ut aliquip ex ea commodo...</div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+
+                                                                </div>
+                                                            </div>
+                                                            <!--END_SC_TEAM -->
+                                                        </div>
+                                                        <div class="clearfix"></div>
+                                                        <!--BEGIN_SC_TEAM_WRAP -->
+                                                        <a href="#" class="sc_button sc_button_square sc_button_style_filled sc_button_size_large margin_top_tiny margin_bottom_small">
+                                                            <span class="cube flip-to-top">
+                                                                <span class="default-state">
+                                                                    <span>Learn More</span>
+                                                                </span>
+                                                                <span class="active-state">
+                                                                    <span>Learn More</span>
+                                                                </span>
+                                                            </span>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>                	        
+            </div>
+        </div>	                
+    </body>
+
+</html>
---
+++ blocks/810/810.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/810.css" rel="stylesheet" type="text/css"/>
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/810.less', 'css/810.css');
+        ?>
+        <script src="js/112.js" type="text/javascript"></script>
+        <script src="js/npm.js" type="text/javascript"></script>
+        <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+
+    </head>
+    <body>
+        <div class="mod-custom-2">
+            <div class="padding_top_7_143em_imp padding_bottom_7_143em_imp theme_background_1">
+            <div class="container">
+                <h3 class="sc_title sc_title_regular sc_align_center margin_top_null margin_bottom_null text_align_center font_weight_600 font_size_3_571em">Club Membership</h3>
+                <h4 class="sc_title sc_title_regular sc_align_center text_uppercase ltr-spc2 margin_top_null margin_bottom_large text_align_center font_weight_600 font_size_0_857em accent1">Select the best plan</h4>
+                <div class="columns_wrap sc_columns columns_nofluid sc_columns_count_4">
+                    <!-- Price block -->
+                    <div class="col-md-3">
+                        <div class="sc_price_block sc_price_block_style_2">
+                            <div class="sc_price_block_title">
+                                <span>Junior</span>
+                            </div>
+                            <div class="sc_price_block_money">
+                                <div class="sc_price">
+                                    <span class="sc_price_currency">$</span>
+                                    <span class="sc_price_money">250</span>
+                                    <span class="sc_price_period">mth</span>
+                                </div>
+                            </div>
+                            <div class="sc_price_block_description">Juniors must be less than 19 years old as at 1st January</div>
+                            <div class="sc_price_block_link">
+                                <a href="#" class="sc_button sc_button_square sc_button_style_filled sc_button_size_large">
+                                    <span class="cube flip-to-top">
+                                        <span class="default-state">
+                                            <span>Order now</span>
+                                        </span>
+                                        <span class="active-state">
+                                            <span>Order now</span>
+                                        </span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Price block -->
+                    <!-- Price block -->
+                    <div class="col-md-3">
+                        <div class="sc_price_block sc_price_block_style_3">
+                            <div class="sc_price_block_title">
+                                <span>Individual</span>
+                            </div>
+                            <div class="sc_price_block_money">
+                                <div class="sc_price">
+                                    <span class="sc_price_currency">$</span>
+                                    <span class="sc_price_money">350</span>
+                                    <span class="sc_price_period">mth</span>
+                                </div>
+                            </div>
+                            <div class="sc_price_block_description">Save $98 every year compared to the monthly plan by paying yearly.</div>
+                            <div class="sc_price_block_link">
+                                <a href="#" class="sc_button sc_button_square sc_button_style_filled sc_button_size_large">
+                                    <span class="cube flip-to-top">
+                                        <span class="default-state">
+                                            <span>Order now</span>
+                                        </span>
+                                        <span class="active-state">
+                                            <span>Order now</span>
+                                        </span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Price block -->
+                    <!-- Price block -->
+                    <div class="col-md-3">
+                        <div class="sc_price_block sc_price_block_style_3">
+                            <div class="sc_price_block_title">
+                                <span>Recommended</span>
+                            </div>
+                            <div class="sc_price_block_money">
+                                <div class="sc_price">
+                                    <span class="sc_price_currency">$</span>
+                                    <span class="sc_price_money">450</span>
+                                    <span class="sc_price_period">mth</span>
+                                </div>
+                            </div>
+                            <div class="sc_price_block_description">Save $128 by inviting your family to train with you monthly</div>
+                            <div class="sc_price_block_link">
+                                <a href="#" class="sc_button sc_button_square sc_button_style_filled sc_button_size_large">
+                                    <span class="cube flip-to-top">
+                                        <span class="default-state">
+                                            <span>Order now</span>
+                                        </span>
+                                        <span class="active-state">
+                                            <span>Order now</span>
+                                        </span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Price block -->
+                    <!-- Price block -->
+                    <div class="col-md-3">
+                        <div class="sc_price_block sc_price_block_style_3">
+                            <div class="sc_price_block_title">
+                                <span>Family</span>
+                            </div>
+                            <div class="sc_price_block_money">
+                                <div class="sc_price">
+                                    <span class="sc_price_currency">$</span>
+                                    <span class="sc_price_money">450</span>
+                                    <span class="sc_price_period">mth</span>
+                                </div>
+                            </div>
+                            <div class="sc_price_block_description">Save $128 by inviting your family to train with you monthly</div>
+                            <div class="sc_price_block_link">
+                                <a href="#" class="sc_button sc_button_square sc_button_style_filled sc_button_size_large">
+                                    <span class="cube flip-to-top">
+                                        <span class="default-state">
+                                            <span>Order now</span>
+                                        </span>
+                                        <span class="active-state">
+                                            <span>Order now</span>
+                                        </span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Price block -->
+                </div>
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
